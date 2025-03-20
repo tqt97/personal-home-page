98688be86cf035a1df3ae0be7d615999d7f3ebf4
tuantq <kutuanonline199@gmail.com>
2025-03-20 21:27:35 +0700

    Delete Task and Shadcn Toast Notification

--- resources/js/pages/Tasks/Index.tsx
+++ resources/js/pages/Tasks/Index.tsx
@@ -1,9 +1,17 @@
+import { Button } from '@/components/ui/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
 import { Task } from '@/types';
-import { Head } from '@inertiajs/react';
+import { Head, router } from '@inertiajs/react';
+import { toast } from 'sonner';
 
 export default function Index({ tasks }: { tasks: Task[] }) {
+    const deleteTask = (task: Task) => {
+        if (confirm('Are you sure?')) {
+            router.delete(route('tasks.destroy', { task }));
+            toast.success('Task deleted successfully');
+        }
+    };
     return (
         <AppLayout>
             <Head title="Tasks List" />
@@ -22,6 +30,11 @@ export default function Index({ tasks }: { tasks: Task[] }) {
                                 <TableCell>{task.name}</TableCell>
                                 <TableCell className={task.is_completed ? 'text-green-600' : 'text-red-700'}>
                                     {task.is_completed ? 'Completed' : 'In Progress'}
+                                </TableCell>
+                                <TableCell className="flex flex-row gap-x-2 text-right">
+                                    <Button variant={'destructive'} className={'cursor-pointer'} onClick={() => deleteTask(task)}>
+                                        Delete
+                                    </Button>
                                 </TableCell>
                             </TableRow>
                         ))}
