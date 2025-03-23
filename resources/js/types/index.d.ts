6f8a039bd79d36077c72af4d4cc07840a88c93e1
tuantq <kutuanonline199@gmail.com>
2025-03-23 09:07:50 +0700

    Add interface BreadcrumbItem

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -40,3 +40,8 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export interface BreadcrumbItem {
+    title: string;
+    href: string;
+}
