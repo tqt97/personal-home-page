6a5421340efcc50b8d8a8ffa5fc883d2bb18b93f
tuantq <kutuanonline199@gmail.com>
2025-03-23 09:19:47 +0700

    Add Shadcn table pagination and helper

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -45,3 +45,23 @@ export interface BreadcrumbItem {
     title: string;
     href: string;
 }
+
+export interface PaginatedResponse<T = User | null> {
+    current_page: number;
+    data: T[];
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: {
+        url: string | null;
+        label: string;
+        active: boolean;
+    }[];
+    next_page_url: string | null;
+    path: string;
+    per_page: number;
+    prev_page_url: string | null;
+    to: number;
+    total: number;
+}
