aa02c531e81934f784af662dd73fc651aac4abe3
tuantq <kutuanonline199@gmail.com>
2025-03-23 09:04:30 +0700

    Add Shadcn Toast Notification

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -1,6 +1,7 @@
 import { AppContent } from '@/components/app-content';
 import { AppHeader } from '@/components/app-header';
 import { AppShell } from '@/components/app-shell';
+import { Toaster } from '@/components/ui/sonner';
 import { type BreadcrumbItem } from '@/types';
 import type { PropsWithChildren } from 'react';
 
@@ -9,6 +10,7 @@ export default function AppHeaderLayout({ children, breadcrumbs }: PropsWithChil
         <AppShell>
             <AppHeader breadcrumbs={breadcrumbs} />
             <AppContent>{children}</AppContent>
+            <Toaster position={'top-right'} />
         </AppShell>
     );
 }
